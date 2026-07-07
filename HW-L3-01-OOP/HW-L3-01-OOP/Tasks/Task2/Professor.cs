52b2fc67cdc990464d105a79e39b6e01a5aebf42
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-07 15:18:00 +0330

    [implement] Professor class

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task2/Professor.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Task2
+{
+    internal class Professor : Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string ProfessorId { get; private set; }
+        public string Subject { get; private set; }
+
+        public Professor(string name, int age, string professorId, string subject) : base(name, age)
+        {
+            Name = name;
+            Age = age;
+            ProfessorId = professorId;
+            Subject = subject;
+        }
+
+        public override void GetDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, professor ID: {ProfessorId}, Subject: {Subject}");
+        }
+    }
+}
