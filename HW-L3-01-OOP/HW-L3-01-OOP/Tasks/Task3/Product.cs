1b69265aa928cc393c5f71c1244cbaa2e44e2ca9
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-07 17:50:38 +0330

    [implement] Product class

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task3/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Tasks.Task3
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public void GetProductDetails() {
+            Console.WriteLine($"Name: {Name}, Price: {Price}");
+        }
+    }
+}
