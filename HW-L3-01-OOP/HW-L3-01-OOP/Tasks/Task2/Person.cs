8b4fb61d88a2cd365fd75cc1693688961ab2797b
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-07 15:13:16 +0330

    [implement] Person & Student class

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task2/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Tasks.Task2
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name , int age) { 
+            Name = name;
+            Age = age;
+        }
+
+        public virtual void GetDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task2/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace Tasks.Task2
+{
+    internal class Student : Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string StudentID { get; private set; }
+        public string Major { get; private set; }
+
+        public Student(string name, int age, string studentID, string major) : base(name, age)
+        {
+            Name = name;
+            Age = age;
+            StudentID = studentID;
+            Major = major;
+        }
+
+        public override void GetDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Student ID: {StudentID}, Major: {Major}");
+        }
+    }
+}
