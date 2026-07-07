1a8ba44b8415d19223af1a38bf261ac45766ce33
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-07 15:24:32 +0330

    [implement] PersonRunner class

--- HW-L3-01-OOP/HW-L3-01-OOP/Program.cs
+++ HW-L3-01-OOP/HW-L3-01-OOP/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Tasks.Task1;
+using Tasks.Task2;
 
 class Program
 {
     static void Main(string[] args)
     {
         BookRunner.Run();
+        PersonRunner.Run();
     }
 }
---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task2/PersonRunner.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.Task2
+{
+    internal class PersonRunner
+    {
+        public static void Run()
+        {
+            List<Person> people = new()
+            {
+                new Student("Ali", 22, "S1001", "Software Engineering"),
+                new Student("Sara", 21, "S1002", "Computer Science"),
+                new Professor("Dr. Smith", 45, "P2001", "Object-Oriented Programming"),
+                new Professor("Dr. Johnson", 52, "P2002", "Database Systems")
+            };
+
+            people.ForEach(person =>
+            {
+                person.GetDetails();
+            });
+        }
+    }
+}
