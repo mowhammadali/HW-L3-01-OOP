c05ba1ddff72a4d0cb7bf9296fefe573f38b16aa
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-08 11:42:27 +0330

    [implement] Doctor class

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Doctor.cs
@@ -0,0 +1,19 @@
+﻿namespace Tasks.Task4
+{
+    internal class Doctor : Person
+    {
+        public string DoctorId { get; private set; }
+        public string Specialization { get; private set; }
+
+        public Doctor(string name, int age, string id, string doctorId, string specialization) : base(name, age, id)
+        {
+            DoctorId = doctorId;
+            Specialization = specialization;
+        }
+
+        public void Diagnose(Patient patient)
+        {
+            patient.AddToMedicalHistory("");
+        }
+    }
+}
