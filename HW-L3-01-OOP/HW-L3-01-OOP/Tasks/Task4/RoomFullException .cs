44bdd1291d9ba4cf1afb499890bf9d9e7ea39a63
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-08 14:58:48 +0330

    [implement] RoomFullException

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/RoomFullException .cs
@@ -0,0 +1,9 @@
+﻿namespace Tasks.Task4
+{
+    public class RoomFullException : Exception
+    {
+        public RoomFullException() : base("Room is full.") { }
+
+        public RoomFullException(string message) : base(message) { }
+    }
+}
