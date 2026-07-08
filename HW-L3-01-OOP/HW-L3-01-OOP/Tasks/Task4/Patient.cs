8d678319bf1904389ef4ca870006fbf40a6a27da
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-08 11:36:21 +0330

    [implement] person & patient

---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace Tasks.Task4
+{
+    internal class Patient : Person
+    {
+        public string PatientId { get; private set; }
+        public List<string> MedicalHistory { get; private set; }
+
+        public Patient(string name, int age, string id, string patientId, List<string> medicalHistory) : base(name, age, id)
+        {
+            PatientId = patientId;
+            MedicalHistory = medicalHistory;
+        }
+
+        public void AddToMedicalHistory(string illness)
+        {
+            MedicalHistory.Add(illness);
+        }
+    }
+}
---
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks.Task4
+{
+    internal class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string NationalId { get; private set; }
+
+        public Person(string name, int age, string id)
+        {
+            Name = name;
+            Age = age;
+            NationalId = id;
+        }
+
+        public string GetDetails()
+        {
+            return $"Name: {Name}, Age: {Age}, NationalId: {NationalId}";
+        }
+    }
+}
