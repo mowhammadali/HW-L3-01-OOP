0b24ed5c2b5afe2092a01ab9a25dc08b27ea3b8c
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-07-08 12:16:10 +0330

    [implement] Doctor class as context strategy pattern

--- HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Doctor.cs
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Doctor.cs
@@ -4,16 +4,24 @@
     {
         public string DoctorId { get; private set; }
         public string Specialization { get; private set; }
+        private IDiagnosisStrategy _diagnosisStrategy;
 
-        public Doctor(string name, int age, string id, string doctorId, string specialization) : base(name, age, id)
+        public Doctor(string name, int age, string id, string doctorId, string specialization , IDiagnosisStrategy diagnosisStrategy) : base(name, age, id)
         {
             DoctorId = doctorId;
             Specialization = specialization;
+            _diagnosisStrategy = diagnosisStrategy;
+        }
+
+        // Strategy Pattern
+        public void SetStrategy(IDiagnosisStrategy strategy)
+        {
+            _diagnosisStrategy = strategy;
         }
 
         public void Diagnose(Patient patient)
         {
-            patient.AddToMedicalHistory("");
+            _diagnosisStrategy.Diagnose(patient);
         }
     }
 }
--- HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Patient.cs
+++ HW-L3-01-OOP/HW-L3-01-OOP/Tasks/Task4/Patient.cs
@@ -5,9 +5,14 @@
         public string PatientId { get; private set; }
         public List<string> MedicalHistory { get; private set; }
 
-        public Patient(string name, int age, string id, string patientId, List<string> medicalHistory) : base(name, age, id)
+        public Patient(string name, int age, string id, string patientId) : base(name, age, id)
         {
             PatientId = patientId;
+            MedicalHistory = new List<string>();
+        }
+
+        public Patient(string name, int age, string id, string patientId, List<string> medicalHistory) : this(name, age, id, patientId)
+        {
             MedicalHistory = medicalHistory;
         }
 
